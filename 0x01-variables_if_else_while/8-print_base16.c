463363be432a63c73057286c653d3abd5469c0d0
Clinton <clintogechi@gmail.com>
2021-08-30 16:19:54 +0300

    Added 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - program that prints all the numbers of base 16 in lowercase, followed by a new line. 
- * 
  * Return: Always (0)
  */
-
 int main(void)
 {
 	int n;
