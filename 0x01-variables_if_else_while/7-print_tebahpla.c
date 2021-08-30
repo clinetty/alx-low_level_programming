8904741f7c4e297595073909920241ae72369cdd
Clinton <clintogechi@gmail.com>
2021-08-30 15:57:22 +0300

    Added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Program that prints the lowercase alphabet line.
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for(i = 'z';i >= 'z'; i--)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
