9b134c77ede9b5ecfee5bc1a7412930d4ed236f4
Clinton <clintogechi@gmail.com>
2021-08-31 20:02:50 +0300

    function that prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,15 +6,9 @@
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	char ch = "Holberton";
+
+
+	_putchar(ch);
 	return (0);
 }
