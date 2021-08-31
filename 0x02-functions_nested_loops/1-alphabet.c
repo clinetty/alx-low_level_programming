78d96c178008f5741312b91e8940c69570284f8c
Clinton <clintogechi@gmail.com>
2021-08-31 20:25:37 +0300

    Added 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,16 @@
  * Return: Always 0 (Success)
  */
 
-
 void print_alphabet(void)
-{	char c;
+{
+	char x;
 
-	for (c = 'a'; c <= 'z'; c++)
-	_putchar(c);
+	x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	}
 	_putchar('\n');
 }
