863c883dbe7dadb407a9f326a0d3de3583f8bbd9
Clinton <clintogechi@gmail.com>
2021-08-31 20:09:51 +0300

    Added 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,9 +6,15 @@
  */
 int main(void)
 {
-	char ch = "Holberton";
-
-
-	_putchar(ch);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
