3a08dfbaf343d9e61a7ff6e8c587aa160d390814
Clinton <clintogechi@gmail.com>
2021-08-31 22:47:55 +0300

    Added 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks for alphabetic character.
+ *
+ * @c: char type letter
+ *
+ * Return: 1 if c is letter, lowercase or uppercase and 0 otherwise
+ */
+
+int _islower(int c)
+{
+        if ((c >= 65 && c <= 91) || (c >=97 && c <= 123))
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+}
