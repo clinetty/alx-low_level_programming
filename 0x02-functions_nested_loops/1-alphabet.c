2281b7a3bc2f5041c74533a5c90ce4c937d7c883
Clinton <clintogechi@gmail.com>
2021-08-31 18:57:58 +0300

    function that prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	x= 'a'
+
+	while (x <= 'z'
+	{
+		_putchar(x);
+		x++;
+	}
+	
+	_putchar('\n');
+}
