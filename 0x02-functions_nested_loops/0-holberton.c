887c188fda1115813c2160491d712338b57673f7
Clinton <clintogechi@gmail.com>
2021-08-31 17:45:52 +0300

    program that prints Holberton, followed by a new line

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include<stio.h>
+
+/**
+ * main - program that prints Holberton, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = "Holberton";
+
+	putchar(ch);
+	return (0);
+
+}
