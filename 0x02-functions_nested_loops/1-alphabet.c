956e909211219c564db76cefb4d3bb3a0deb78da
Clinton <clintogechi@gmail.com>
2021-08-31 20:19:35 +0300

    Added 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,16 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - check the code for ALX School students.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 
+
 void print_alphabet(void)
-{
-	char x;
+{	char c;
 
-	x= 'a'
-
-	while (x <= 'z'
-	{
-		_putchar(x);
-		x++;
-	}
-	
+	for (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
 	_putchar('\n');
 }
