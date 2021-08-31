36e5b7716e43e5591e550f038802fea0e70d1805
Clinton <clintogechi@gmail.com>
2021-08-31 22:49:05 +0300

    Added 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,7 +8,7 @@
  * Return: 1 if c is letter, lowercase or uppercase and 0 otherwise
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
         if ((c >= 65 && c <= 91) || (c >=97 && c <= 123))
         {
