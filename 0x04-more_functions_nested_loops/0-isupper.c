48d7561c3d14d832141714e7c73bf3966cd0c533
Clinton <clintogechi@gmail.com>
2021-09-02 18:21:12 +0300

    Added 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - checks for uppercase character
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isupper(int c)
+{
+	int c;
+
+	if (c >=65 && c<= 90)
+	{
+		return (1);
+	}
+	else 
+		return (0);
+}
