edeb5047b2d4527323c39f1a31e6f4c3afe60c6d
clinetty <clintogechi@gmail.com>
2021-09-13 21:40:04 +0300

    Added 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,20 @@
 include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * @s: pointer to string
- * @b: constant byte
- * @n: first bytes to change
- * Return: pointer to modified string
+ * _memset - fills memory with a constant byte.
+ * @s: mem area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ * Return: pointer to the mem area s
  */
-
+ 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
+	{
 		s[i] = b;
+	}
 	return (s);
 }
