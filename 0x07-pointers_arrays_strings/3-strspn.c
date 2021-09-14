0cdbeb44668c5f51dd64021b7b45a8f0b1fbf8b1
clinetty <clintogechi@gmail.com>
2021-09-14 23:40:44 +0300

    Added 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,30 +1,36 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of a prefix substring
- *
- * @s: string to work on
- * @accept: pattern to match against
- * Return: unsigned int to how many initial bytes match the pattern
+ * _strspn - search a string for a set of bytes
+ * @s: char string array
+ * @accept: char array to check bytes with
+ * Return: Number of bytes in the intial segment of `s`
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int bytes = 0;
 	int i;
+	int j;
+	int c;
 
-	while (*s)
+	i = 0;
+	c = 0;
+
+	while (s[i] != '\0')
 	{
-		for (i = 0; accept[i]; i++)
+		j = 0;
+		while (accept[j] != '\0')
 		{
-			if (*s == accept[i])
+			if (s[i] == accept[j])
 			{
-				bytes++;
+				c++;
 				break;
 			}
-			else if (accept[i + 1] == '\0')
-				return (bytes);
+			j++;
 		}
-		s++;
+		if (accept[j] == '\0')
+			break;
+		i++;
 	}
-	return (bytes);
+	return (c);
 }
