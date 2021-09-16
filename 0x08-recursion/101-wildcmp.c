8c1cd6d2739ab380ec5639340fca54aeb84b32bd
clinetty <clintogechi@gmail.com>
2021-09-17 00:24:56 +0300

    Added 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,19 +1,66 @@
 #include "main.h"
+
+int check_path(char *S1, char *s2);
+char *move_char(char *s2);
+
 /**
- * wildcmp - A function that compares two strings
- * @s1: First input string
- * @s2: Second input string
- * Return: 1 if strings identical and 0 in otherwise
+ * wildcmp - compares 2 strings
+ * @s1: string to be compared
+ * @s2: string to be compared
+ * Return: always 0
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (!*s1 && !*s2)
+	int i = 0;
+
+	if (*s1 == '\0' && *s2 == '*' && !*move_char(s2))
 		return (1);
 	if (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (1);
 		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-		return (1);
-	if (*s2 == '*' && *(s1 + 1) && *s2)
+	}
+	if (*s1 == '\0' || *s2 == '\0')
 		return (0);
+	if (*s2 == '*')
+	{
+		s2 = move_char(s2);
+		if (*s2 == '\0')
+			return (1);
+		if (*s1 == *s2)
+			i += wildcmp(s1 + 1, s2 + 1);
+		i += check_path(s1 + 1, s2);
+		return (!!i);
+	}
 	return (0);
+}
+
+/**
+ *check_path - checks recursively for all the paths when the char are equal
+ * @s1: first string
+ * @s2: second string
+ * Return: return value of wildcmp() or of itself
+ */
+int check_path(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+		return (0);
+	if (*s1 == *s2)
+		return (wildcmp(s1, s2));
+	return  (check_path(s1 + 1, s2));
+
+}
+
+/**
+ * *move_char - moves the current char past the *
+ * @s2: string to iterate over
+ * Return: the address of the character after the *
+ */
+char *move_char(char *s2)
+{
+	if (*s2 == '*')
+		return (move_char(s2 + 1));
+	else
+		return (s2);
 }
