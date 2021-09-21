d4e468b159663bede72121a32145dfaf5c057973
clinetty <clintogechi@gmail.com>
2021-09-21 09:29:07 +0300

    Added 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,12 +7,11 @@
  * @argv: argument vector
  * Return: int
  */
-
 int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[]);
+		printf("%s\n", argv[i]);
 	return (0);
 }
